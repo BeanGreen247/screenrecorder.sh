caee21a78b0323024917dee46d6429e30b93fcc9
Tomáš Mozdřeň <46648569+BeanGreen247@users.noreply.github.com>
2020-07-19 10:16:43 +0200

    small improvements

--- screenrecorder.sh
+++ screenrecorder.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+pulseaudio -k
 pkill -f /dev/video0
-mpv --geometry=-0-0 --autofit=30% --untimed /dev/video0 & ffmpeg -y -f x11grab -s 1680x1050 -i :0.0+1280 -f alsa -i default screenout.mp4  
+mpv --geometry=-0-0 --autofit=30% --untimed /dev/video0 & ffmpeg -y -f x11grab -s 1680x1050 -i :0.0+1280 -threads 0 -f alsa -i default screenout.mp4
